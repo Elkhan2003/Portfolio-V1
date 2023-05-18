5ed7f863c042021ceb29011117f302f440eae0af
Elkhan Sharshenbekov <98739225+Elkhan2003@users.noreply.github.com>
2023-05-18 13:36:32 +0600

    Added files to GitHub🎶

--- src/components/pages/ProjectPage.tsx
+++ src/components/pages/ProjectPage.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import scss from "./Style.module.scss";
 import Image from "next/image";
 import project_1 from "@/assets/projects/coindom.png";
+import { ArrowUpRightIcon, GithubIcon } from "@/components/svgs";
 
 const ProjectPage: FC = () => {
 	return (
@@ -15,21 +16,118 @@ const ProjectPage: FC = () => {
 								Each project is a unique piece of development 🧩
 							</h3>
 						</div>
+
+						{/* ! card 1 */}
 						<div className={scss.card}>
 							<div className={scss.pro__text}>
-								<h3>COINDOM 🪙</h3>
-								<p>
+								<h3 className={`${scss.text__z__index}`}>COINDOM 🪙</h3>
+								<p className={`${scss.text__z__index}`}>
 									Coindom is a crypto app that allows users to search for
 									information about various cryptocurrencies in real-time.
 								</p>
 								<div className={scss.stack}>
-									<p>React</p>
-									<p>SCSS</p>
+									<p className={`${scss.text__z__index}`}>React</p>
+									<p className={`${scss.text__z__index}`}>SCSS</p>
+								</div>
+								<div className={scss.links}>
+									<a
+										className={`${scss.text__z__index} ${scss.code}`}
+										href="#"
+										target="_blank"
+									>
+										Code
+										<GithubIcon />
+									</a>
+									<a
+										className={`${scss.text__z__index} ${scss.demo}`}
+										href="#"
+										target="_blank"
+									>
+										Live Demo
+										<ArrowUpRightIcon />
+									</a>
 								</div>
 							</div>
-							<div className={scss.pro__img}>
+							<div className={`${scss.text__z__index} ${scss.pro__img}`}>
 								<a href="#" target="_blank">
-									{/*<Image src={project_1} alt={"project"} />*/}
+									<Image src={project_1} alt={"project"} />
+								</a>
+							</div>
+						</div>
+
+						{/* ! card 2 */}
+						<div className={`${scss.card} ${scss.row__reverse}`}>
+							<div className={scss.pro__text}>
+								<h3 className={`${scss.text__z__index}`}>COINDOM 🪙</h3>
+								<p className={`${scss.text__z__index}`}>
+									Coindom is a crypto app that allows users to search for
+									information about various cryptocurrencies in real-time.
+								</p>
+								<div className={scss.stack}>
+									<p className={`${scss.text__z__index}`}>React</p>
+									<p className={`${scss.text__z__index}`}>SCSS</p>
+								</div>
+								<div className={scss.links}>
+									<a
+										className={`${scss.text__z__index} ${scss.code}`}
+										href="#"
+										target="_blank"
+									>
+										Code
+										<GithubIcon />
+									</a>
+									<a
+										className={`${scss.text__z__index} ${scss.demo}`}
+										href="#"
+										target="_blank"
+									>
+										Live Demo
+										<ArrowUpRightIcon />
+									</a>
+								</div>
+							</div>
+							<div className={`${scss.text__z__index} ${scss.pro__img}`}>
+								<a href="#" target="_blank">
+									2
+									<Image src={project_1} alt={"project"} />
+								</a>
+							</div>
+						</div>
+
+						{/* ! card 3 */}
+						<div className={scss.card}>
+							<div className={scss.pro__text}>
+								<h3 className={`${scss.text__z__index}`}>COINDOM 🪙</h3>
+								<p className={`${scss.text__z__index}`}>
+									Coindom is a crypto app that allows users to search for
+									information about various cryptocurrencies in real-time.
+								</p>
+								<div className={scss.stack}>
+									<p className={`${scss.text__z__index}`}>React</p>
+									<p className={`${scss.text__z__index}`}>SCSS</p>
+								</div>
+								<div className={scss.links}>
+									<a
+										className={`${scss.text__z__index} ${scss.code}`}
+										href="#"
+										target="_blank"
+									>
+										Code
+										<GithubIcon />
+									</a>
+									<a
+										className={`${scss.text__z__index} ${scss.demo}`}
+										href="#"
+										target="_blank"
+									>
+										Live Demo
+										<ArrowUpRightIcon />
+									</a>
+								</div>
+							</div>
+							<div className={`${scss.text__z__index} ${scss.pro__img}`}>
+								<a href="#" target="_blank">
+									<Image src={project_1} alt={"project"} />
 								</a>
 							</div>
 						</div>
--- src/components/svgs/index.ts
+++ src/components/svgs/index.ts
@@ -1,5 +1,6 @@
 export { default as ArrowIcon } from "./arrow.svg";
 export { default as LangIcon } from "./lang.svg";
+export { default as ArrowUpRightIcon } from "./arrow-up-right-from-square-solid.svg";
 export { default as SunIcon } from "./sun.svg";
 export { default as MoonIcon } from "./moon.svg";
 export { default as PhoneIcon } from "./phone.svg";
