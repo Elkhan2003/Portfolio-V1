49808dc4c41a05a276772f1cb49fb0bebbaed396
Elkhan Sharshenbekov <98739225+Elkhan2003@users.noreply.github.com>
2023-05-18 18:51:24 +0600

    Rewrote the projects page to map rendering😈

--- src/components/pages/ProjectPage.tsx
+++ src/components/pages/ProjectPage.tsx
@@ -4,7 +4,42 @@ import Image from "next/image";
 import project_1 from "@/assets/projects/coindom.png";
 import { ArrowUpRightIcon, GithubIcon } from "@/components/svgs";
 
+interface projectsTypes {
+	title: string;
+	description: string;
+	stack: Array<string>;
+	codeLink: string;
+	demoLink: string;
+}
+
 const ProjectPage: FC = () => {
+	const projects: projectsTypes[] = [
+		{
+			title: "COINDOM 🪙",
+			description:
+				"Coindom is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
+			stack: ["React", "SCSS"],
+			codeLink: "#",
+			demoLink: "#"
+		},
+		{
+			title: "COINDOM 🪙",
+			description:
+				"Coindom is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
+			stack: ["React", "SCSS"],
+			codeLink: "#",
+			demoLink: "#"
+		},
+		{
+			title: "COINDOM 🪙",
+			description:
+				"Coindom is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
+			stack: ["React", "SCSS"],
+			codeLink: "#",
+			demoLink: "#"
+		}
+	];
+
 	return (
 		<>
 			<div id="project" className={scss.project__page}>
@@ -17,141 +52,62 @@ const ProjectPage: FC = () => {
 							</h3>
 						</div>
 
-						{/* ! card 1 */}
-						<div className={scss.card}>
-							<div className={scss.pro__text}>
-								<h3 className={`${scss.text__z__index}`}>COINDOM 🪙</h3>
-								<p className={`${scss.text__z__index}`}>
-									Coindom is a crypto app that allows users to search for
-									information about various cryptocurrencies in real-time.
-								</p>
-								<div className={scss.stack}>
-									<p className={`${scss.text__z__index}`}>React</p>
-									<p className={`${scss.text__z__index}`}>SCSS</p>
+						{projects.map((project, index) => (
+							<div
+								className={`${scss.card} ${
+									index === 1 ? scss.row__reverse : ""
+								}`}
+								key={index + 1}
+							>
+								<div className={scss.pro__text}>
+									<h3 className={`${scss.text__z__index}`}>{project.title}</h3>
+									<p className={`${scss.text__z__index}`}>
+										{project.description}
+									</p>
+									<div className={scss.stack}>
+										{project.stack.map((item, index) => (
+											<p className={`${scss.text__z__index}`} key={index + 1}>
+												{item}
+											</p>
+										))}
+									</div>
+									<div className={scss.links}>
+										<a
+											className={`${scss.text__z__index} ${scss.code}`}
+											href={project.codeLink}
+											target="_blank"
+										>
+											Code
+											<GithubIcon />
+										</a>
+										<a
+											className={`${scss.text__z__index} ${scss.demo}`}
+											href={project.demoLink}
+											target="_blank"
+										>
+											Live Demo
+											<ArrowUpRightIcon />
+										</a>
+									</div>
 								</div>
-								<div className={scss.links}>
-									<a
-										className={`${scss.text__z__index} ${scss.code}`}
-										href="#"
-										target="_blank"
-									>
-										Code
-										<GithubIcon />
-									</a>
-									<a
-										className={`${scss.text__z__index} ${scss.demo}`}
-										href="#"
-										target="_blank"
-									>
-										Live Demo
-										<ArrowUpRightIcon />
-									</a>
-								</div>
-							</div>
-							<div className={`${scss.text__z__index} ${scss.pro__img}`}>
-								<a href="#" target="_blank">
-									<Image
-										priority={true}
-										quality={25}
-										loading="eager"
-										src={project_1}
-										alt={"project"}
-									/>
-								</a>
-							</div>
-						</div>
-
-						{/* ! card 2 */}
-						<div className={`${scss.card} ${scss.row__reverse}`}>
-							<div className={scss.pro__text}>
-								<h3 className={`${scss.text__z__index}`}>COINDOM 🪙</h3>
-								<p className={`${scss.text__z__index}`}>
-									Coindom is a crypto app that allows users to search for
-									information about various cryptocurrencies in real-time.
-								</p>
-								<div className={scss.stack}>
-									<p className={`${scss.text__z__index}`}>React</p>
-									<p className={`${scss.text__z__index}`}>SCSS</p>
-								</div>
-								<div className={scss.links}>
-									<a
-										className={`${scss.text__z__index} ${scss.code}`}
-										href="#"
-										target="_blank"
-									>
-										Code
-										<GithubIcon />
-									</a>
-									<a
-										className={`${scss.text__z__index} ${scss.demo}`}
-										href="#"
-										target="_blank"
-									>
-										Live Demo
-										<ArrowUpRightIcon />
+								<div className={`${scss.text__z__index} ${scss.pro__img}`}>
+									<a href="#" target="_blank">
+										<Image
+											priority={true}
+											quality={25}
+											loading="eager"
+											src={project_1}
+											alt={"project"}
+										/>
 									</a>
 								</div>
 							</div>
-							<div className={`${scss.text__z__index} ${scss.pro__img}`}>
-								<a href="#" target="_blank">
-									<Image
-										priority={true}
-										quality={25}
-										loading="eager"
-										src={project_1}
-										alt={"project"}
-									/>
-								</a>
-							</div>
-						</div>
-
-						{/* ! card 3 */}
-						<div className={scss.card}>
-							<div className={scss.pro__text}>
-								<h3 className={`${scss.text__z__index}`}>COINDOM 🪙</h3>
-								<p className={`${scss.text__z__index}`}>
-									Coindom is a crypto app that allows users to search for
-									information about various cryptocurrencies in real-time.
-								</p>
-								<div className={scss.stack}>
-									<p className={`${scss.text__z__index}`}>React</p>
-									<p className={`${scss.text__z__index}`}>SCSS</p>
-								</div>
-								<div className={scss.links}>
-									<a
-										className={`${scss.text__z__index} ${scss.code}`}
-										href="#"
-										target="_blank"
-									>
-										Code
-										<GithubIcon />
-									</a>
-									<a
-										className={`${scss.text__z__index} ${scss.demo}`}
-										href="#"
-										target="_blank"
-									>
-										Live Demo
-										<ArrowUpRightIcon />
-									</a>
-								</div>
-							</div>
-							<div className={`${scss.text__z__index} ${scss.pro__img}`}>
-								<a href="#" target="_blank">
-									<Image
-										priority={true}
-										quality={25}
-										loading="eager"
-										src={project_1}
-										alt={"project"}
-									/>
-								</a>
-							</div>
-						</div>
+						))}
 					</div>
 				</div>
 			</div>
 		</>
 	);
 };
+
 export default ProjectPage;
