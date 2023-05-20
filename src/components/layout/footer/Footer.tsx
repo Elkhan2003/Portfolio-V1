a5adb2e91682a2031d0f7379cc24aca2e9cf840d
Elkhan Sharshenbekov <98739225+Elkhan2003@users.noreply.github.com>
2023-05-21 00:51:38 +0600

    Added Footer 😀

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,10 +1,34 @@
 import React, { FC } from "react";
+import scss from "./Footer.module.scss";
+import { GithubIcon, LinkedinIcon } from "@/components/svgs";
 
 const Footer: FC = () => {
 	return (
 		<>
-			<div>
-				<h3>Footer</h3>
+			<div className={`${scss.footer__page} ${scss.text__z__index}`}>
+				<div className="container">
+					<div className={scss.content}>
+						<h4>Copyright © 2023. All rights are reserved</h4>
+						<div className={`${scss.my__icons}`}>
+							<a
+								className={scss.icon}
+								href="https://github.com/Elkhan2003"
+								target="_blank"
+								aria-label="Linkedin"
+							>
+								<LinkedinIcon />
+							</a>
+							<a
+								className={scss.icon}
+								href="https://github.com/Elkhan2003"
+								target="_blank"
+								aria-label="GitHub"
+							>
+								<GithubIcon />
+							</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
