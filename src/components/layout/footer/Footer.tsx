f0b324173560a65d797e2409ceec049d9803e91d
Elkhan Sharshenbekov <98739225+Elkhan2003@users.noreply.github.com>
2023-05-21 01:21:22 +0600

    Update Footer Style😈

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -9,6 +9,14 @@ const Footer: FC = () => {
 				<div className="container">
 					<div className={scss.content}>
 						<h4>Copyright © 2023. All rights are reserved</h4>
+						<h4 className={scss.center__text}>
+							Built with<span className={scss.heart}>♡</span>by
+							<span>
+								<a href="https://nextjs.org/" target="_blank">
+									NextJS
+								</a>
+							</span>
+						</h4>
 						<div className={`${scss.my__icons}`}>
 							<a
 								className={scss.icon}
