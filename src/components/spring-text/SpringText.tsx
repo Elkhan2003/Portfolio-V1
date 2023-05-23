e3f4f57812932b9183337e8a1d59fef51ca031f8
Elkhan Sharshenbekov <98739225+Elkhan2003@users.noreply.github.com>
2023-05-23 20:51:05 +0600

    Add word Sysadmin in Typing

--- src/components/spring-text/SpringText.tsx
+++ src/components/spring-text/SpringText.tsx
@@ -10,7 +10,8 @@ const SpringText: FC = () => {
 		intl.formatMessage({ id: "page.home.title_2" }),
 		intl.formatMessage({ id: "page.home.title_3" }),
 		intl.formatMessage({ id: "page.home.title_4" }),
-		intl.formatMessage({ id: "page.home.title_5" })
+		intl.formatMessage({ id: "page.home.title_5" }),
+		intl.formatMessage({ id: "page.home.title_6" })
 	];
 
 	return (
