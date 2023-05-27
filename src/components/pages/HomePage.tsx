61152934aefe7fa578a2085999676b1124a825ea
Elkhan Sharshenbekov <98739225+Elkhan2003@users.noreply.github.com>
2023-05-27 20:23:59 +0600

    Added animation for HomePage😍

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
 import scss from "@/components/pages/Style.module.scss";
 import SpringText from "@/components/spring-text/SpringText";
+import { FormattedMessage } from "react-intl";
 import { GithubIcon, LinkedinIcon } from "@/components/svgs";
 import Image, { StaticImageData } from "next/image";
 import * as TechIcons from "@/assets/tech-icons";
-import { FormattedMessage } from "react-intl";
+import { motion } from "framer-motion";
 
 const HomePage: FC = () => {
 	const tech_icons: StaticImageData[] = [
@@ -19,60 +20,138 @@ const HomePage: FC = () => {
 		TechIcons.scss_icon
 	];
 
+	const animationText = {
+		hidden: {
+			x: -100,
+			opacity: 0
+		},
+		visible: (custom: number) => ({
+			x: 0,
+			opacity: 1,
+			transition: { delay: custom * 0.3, else: "easyOut", duration: 0.5 }
+		})
+	};
+
+	const animationIcons = {
+		hidden: {
+			y: 100,
+			opacity: 0
+		},
+		visible: (custom: number) => ({
+			y: 0,
+			opacity: 1,
+			transition: { delay: custom * 0.1, else: "easyOut", duration: 0.4 }
+		})
+	};
+
+	const animationImage = {
+		hidden: {
+			y: -70,
+			opacity: 0
+		},
+		visible: (custom: number) => ({
+			y: 0,
+			opacity: 1,
+			transition: {
+				y: {
+					delay: custom * 0.2,
+					ease: "easeOut",
+					duration: 0.2
+				},
+				opacity: {
+					delay: custom * 0.3,
+					ease: "easeOut",
+					duration: 1
+				}
+			}
+		})
+	};
+
 	return (
 		<>
-			<div id="/" className={scss.home__page}>
+			<motion.div
+				initial="hidden"
+				whileInView="visible"
+				id="/"
+				className={scss.home__page}
+			>
 				<div className="container">
 					<div className={scss.content}>
 						<div className={scss.top}>
 							<div className={scss.left}>
-								<h1 className={`${scss.text__z__index} ${scss.my__name}`}>
+								<motion.h1
+									custom={1}
+									variants={animationText}
+									className={`${scss.text__z__index} ${scss.my__name}`}
+								>
 									<FormattedMessage
 										id="page.home.my_name"
 										values={{ span: (chunks) => <span>{chunks}</span> }}
 									/>
-								</h1>
-								<h1 className={`${scss.text__z__index} ${scss.my__experience}`}>
+								</motion.h1>
+								<motion.h1
+									custom={2}
+									variants={animationText}
+									className={`${scss.text__z__index} ${scss.my__experience}`}
+								>
 									<FormattedMessage id="page.home.my__experience" />
 									&nbsp;
 									<SpringText />
-								</h1>
-								<p className={`${scss.text__z__index} ${scss.my__self}`}>
+								</motion.h1>
+								<motion.p
+									custom={3}
+									variants={animationText}
+									className={`${scss.text__z__index} ${scss.my__self}`}
+								>
 									<FormattedMessage id="page.home.my__self" />
-								</p>
+								</motion.p>
 								<div className={`${scss.text__z__index} ${scss.my__icons}`}>
-									<a
+									<motion.a
+										custom={4}
+										variants={animationIcons}
 										className={scss.icon}
 										href="https://github.com/Elkhan2003"
 										target="_blank"
 										aria-label="Linkedin"
 									>
 										<LinkedinIcon />
-									</a>
-									<a
+									</motion.a>
+									<motion.a
+										custom={6}
+										variants={animationIcons}
 										className={scss.icon}
 										href="https://github.com/Elkhan2003"
 										target="_blank"
 										aria-label="GitHub"
 									>
 										<GithubIcon />
-									</a>
+									</motion.a>
 								</div>
 							</div>
 							<div className={scss.right}>
-								<div className={`${scss.shape}`}></div>
+								<motion.div
+									custom={1}
+									variants={animationImage}
+									className={`${scss.shape}`}
+								></motion.div>
 							</div>
 						</div>
 
 						{/*	! bottom */}
 						<div className={scss.bottom}>
 							<div className={`${scss.my__skills}`}>
-								<p className={`${scss.text__z__index}`}>
+								<motion.p
+									custom={1}
+									variants={animationText}
+									className={`${scss.text__z__index}`}
+								>
 									<FormattedMessage id="page.home.tech_stack" />
-								</p>
+								</motion.p>
 								<div className={scss.logos}>
 									{tech_icons.map((item, index) => (
-										<div
+										<motion.div
+											custom={index + 1}
+											variants={animationIcons}
 											key={index + 1}
 											className={`${scss.text__z__index} ${scss.icon}`}
 										>
@@ -84,14 +163,14 @@ const HomePage: FC = () => {
 												src={item}
 												alt={"icon"}
 											/>
-										</div>
+										</motion.div>
 									))}
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</motion.div>
 		</>
 	);
 };
